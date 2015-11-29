80b8fb33d89970fca60b876804312216a4855d20
Mario Neises <git@mnio.net>
2015-11-29 12:49:24 +0100

    project init

---
+++ 03-SharkAndroidStack/app/src/main/java/de/htw_berlin/sharkandroidstack/MainActivity.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.sharkandroidstack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
