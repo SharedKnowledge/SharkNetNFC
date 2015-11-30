a314b2f45fe5d9c468c8a9181c14d9a388305f66
Mario Neises <git@mnio.net>
2015-11-30 15:40:04 +0100

    stop engine

--- 03-SharkAndroidStack/app/src/main/java/de/htw_berlin/sharkandroidstack/setup/SharkStack.java
+++ 03-SharkAndroidStack/app/src/main/java/de/htw_berlin/sharkandroidstack/setup/SharkStack.java
@@ -37,4 +37,8 @@ public class SharkStack {
         _kb.addListener(_kbTextViewWriter);
         //_kbTextViewWriter.writeKbToTextView(_kb);
     }
+
+    public void stop() {
+        _engine.stop();
+    }
 }
