14917cf85ce329951a2ce4b5c5aed4447e119cfe
Mario Neises <git@mnio.net>
2015-11-30 16:16:44 +0100

    add layout

--- 03-SharkAndroidStack/app/src/main/java/de/htw_berlin/sharkandroidstack/android/MainActivity.java
+++ 03-SharkAndroidStack/app/src/main/java/de/htw_berlin/sharkandroidstack/android/MainActivity.java
@@ -1,7 +1,9 @@
 package de.htw_berlin.sharkandroidstack.android;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import de.htw_berlin.sharkandroidstack.R;
 import de.htw_berlin.sharkandroidstack.setup.SharkStack;
@@ -9,11 +11,28 @@ import de.htw_berlin.sharkandroidstack.setup.SharkStack;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static SharkStack sharkStack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new SharkStack(this);
+        String deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        TextView nameView = (TextView) findViewById(R.id.nameTextView);
+        nameView.setText("Name: " + deviceId);
+
+        if (sharkStack == null) {
+            sharkStack = new SharkStack(this, deviceId);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (sharkStack != null) {
+            sharkStack.stop();
+            sharkStack = null;
+        }
     }
 }
--- 03-SharkAndroidStack/app/src/main/java/de/htw_berlin/sharkandroidstack/setup/SharkStack.java
+++ 03-SharkAndroidStack/app/src/main/java/de/htw_berlin/sharkandroidstack/setup/SharkStack.java
@@ -1,30 +1,28 @@
 package de.htw_berlin.sharkandroidstack.setup;
 
 import android.content.Context;
-import android.provider.Settings;
 import android.util.Log;
 
 import net.sharkfw.knowledgeBase.sync.SyncKB;
 import net.sharkfw.knowledgeBase.sync.SyncKP;
 
 import de.htw_berlin.sharkandroidstack.android.KbTextViewWriter;
+import de.htw_berlin.sharkandroidstack.sharkFW.peer.AndroidSharkEngine;
 
 public class SharkStack {
 
-    private net.sharkfw.peer.AndroidSharkEngine _engine;
+    private AndroidSharkEngine _engine;
     private KbTextViewWriter _kbTextViewWriter;
     private SyncKB _kb;
     private SyncKP _kp;
     private MySimpleKp _wifiKp;
 
-    public SharkStack(Context context) {
-        _engine = new net.sharkfw.peer.AndroidSharkEngine(context);
+    public SharkStack(Context context, String name) {
+        _engine = new AndroidSharkEngine(context);
         _engine.setConnectionTimeOut(20000); //TODO: needed?
 
-        String deviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
-
         try {
-            _kb = new KnowledgeBaseCreator().getKb(deviceId);
+            _kb = new KnowledgeBaseCreator().getKb(name);
             _kp = new SyncKP(_engine, _kb, 1000);
         } catch (net.sharkfw.system.SharkException e) {
             Log.d("Internal", "Setting up the SyncKB failed.");
