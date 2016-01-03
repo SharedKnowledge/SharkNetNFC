0fc91fbaf7ea0484eafe1b5dddb1fc328e9ac51f
Mario Neises <git@mnio.net>
2016-01-03 19:56:55 +0100

    renaming/ cleanup

--- 03-SharkAndroidStack/app/src/main/java/de/htw_berlin/sharkandroidstack/modules/mariodemo/MarioDemoMainActivity.java
+++ 03-SharkAndroidStack/app/src/main/java/de/htw_berlin/sharkandroidstack/modules/mariodemo/MarioDemoMainActivity.java
@@ -2,6 +2,7 @@ package de.htw_berlin.sharkandroidstack.modules.mariodemo;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import de.htw_berlin.sharkandroidstack.R;
 import de.htw_berlin.sharkandroidstack.android.ParentActivity;
@@ -13,16 +14,30 @@ public class MarioDemoMainActivity extends ParentActivity {
         super.onCreate(savedInstanceState);
 
         setLayoutResource(R.layout.module_mariodemo_activity);
+
+        //action bar menu 1/2:
         setOptionsMenu(R.menu.module_mariodemo_menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        //action bar menu 2/2:
         int id = item.getItemId();
 
-//        if (id == R.id...) {
-//            return true;
-//        }
+        String msg = "";
+        switch (id) {
+            case R.id.mariodemo_menu_item_details:
+                msg = "Details";
+                break;
+            case R.id.mariodemo_menu_item_example:
+                msg = "Example";
+                break;
+        }
+
+        if (!msg.isEmpty()) {
+            Toast.makeText(this, "You pressed on '" + msg + "'", Toast.LENGTH_SHORT).show();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
