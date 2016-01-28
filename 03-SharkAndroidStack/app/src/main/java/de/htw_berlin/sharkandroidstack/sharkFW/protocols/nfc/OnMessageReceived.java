29720cc4ada5421cf22ccff7de36dc13b80a17dd
Mario Neises <git@mnio.net>
2016-01-28 19:41:58 +0100

    removed old projects

--- 03-SharkAndroidStack/app/src/main/java/de/htw_berlin/sharkandroidstack/sharkFW/protocols/nfc/OnMessageReceived.java
+++
@@ -1,8 +0,0 @@
-package de.htw_berlin.sharkandroidstack.sharkFW.protocols.nfc;
-
-public interface OnMessageReceived {
-
-    void onMessage(byte[] message);
-
-    void onError(Exception exception);
-}
